0e745e74f1f8352ff4a7e26d24b19c9f1eaff348
minsugoon <zell80@naver.com>
2026-06-29 23:08:55 +0900

    PWA 오류 수정

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-const withPWA = require("next-pwa")({
+const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
   skipWaiting: true,
+  reloadOnOnline: true,
+  cacheOnFrontEndNav: false,
+  fallbacks: false,
 });
 
 import type { NextConfig } from "next";
